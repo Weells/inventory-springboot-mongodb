bafa31754143c662edb527cb09315879db254838
weells <inthemiddleh@gmail.com>
2023-09-15 23:50:10 -0300

    Projeto criado

---
+++ src/main/java/com/brunooliveira/inventoryspringbootmongodb/InventorySpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.brunooliveira.inventoryspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunooliveira/inventoryspringbootmongodb/InventorySpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunooliveira.inventoryspringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
